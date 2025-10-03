4daec84abd9c84e3fd1536e8c3312051a38e7f92
Justice Mubayiwa <j.mubayiwa@gmail.com>
2025-10-03 10:50:42 -0600

    chore: add Netlify config, pin Next version, add eslint-config-next, remove deprecated Tailwind darkMode

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,6 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
     './components/**/*.{js,jsx,ts,tsx}'
   ],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
